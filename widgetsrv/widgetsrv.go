c6bf6ca62cea1c783885ca7a8a45f4ea6cd1e771
Ramiro Gomez <web@ramiro.org>
2012-01-30 00:16:30 +0100

    testing the urlfetch service

--- widgetsrv/widgetsrv.go
+++ widgetsrv/widgetsrv.go
@@ -1,16 +1,36 @@
 package widgetsrv
 
 import (
+    "appengine"
+    "appengine/urlfetch"
     "fmt"
     "http"
 )
 
 func init() {
     http.HandleFunc("/", index)
+    http.HandleFunc("/url", getURL)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, startPage)
+}
+
+func getURL(w http.ResponseWriter, r *http.Request) {
+    c := appengine.NewContext(r)
+    client := urlfetch.Client(c)
+    resp, err := client.Get("http://www.torlaune.de/")
+    if err != nil {
+        http.Error(w, err.String(), http.StatusInternalServerError)
+        return
+    }
+
+    contentlen := resp.ContentLength
+    buffer := make([]byte, contentlen)
+    resp.Body.Read(buffer)
+    w.Header().Set("Content-Length", fmt.Sprint(contentlen))
+    w.Header().Set("Content-Type", resp.Header.Get("Content-Type"))
+    w.Write(buffer)
 }
 
 const startPage = `
